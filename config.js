f8469b8b8739896375d22f9ddfe96d29f7366b0d
charles <charles73chen@gmail.com>
2023-12-16 20:59:54 +0800

    1

---
+++ config.js
@@ -0,0 +1,14 @@
+const ServerSetting = {
+    host: "rtspcameratest.ddns.net",
+    port: 8080,
+    serverPort:300,
+    username: "admin",
+    userpass: "abcd1234",
+    videoWidth: 640,
+    videoHeight: 360,
+    videoWH: '640x480',
+    camerLength: 2,
+    多線程: 4
+}
+
+module.exports = ServerSetting;
