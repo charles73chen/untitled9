a0d39029db599f7ef15650955202ba7ebd0b709e
charles <charles73chen@gmail.com>
2023-12-21 23:19:42 +0800

    1

--- hls.config.js
+++ hls.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'hls',
       watch: true,
       script: './hls.js',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      log_date_format: 'YYYY-MM-DD HH:mm:SS',
       ignore_watch: ['node_modules', '.git', '*.log', '*.html', '.idea', 'source-m3u8', 'log', '.idea', '.txt', '.bak'],
     },
   ],
